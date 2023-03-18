d8a1a4cf147a9f5f07b42f411037984a2eef8acc
Marco Felipe Orellana <mafeorellana09@gmail.com>
2023-03-18 15:28:07 -0300

    update en index y apartados desactualizados ahora tomando nuevos campos en productos.php

--- CristaleriasToro/Login.php
+++ CristaleriasToro/Login.php
@@ -49,17 +49,17 @@
     <div class="row gx-0 d-none d-lg-flex">
         <div class="col-lg-7 px-5 text-start">
             <div class="h-100 d-inline-flex align-items-center me-4">
-                <small class="fa fa-map-marker-alt text-primary me-2"></small>
+                <small class="fa fa-map-marker-alt text-warning me-2"></small>
                 <small>Dagoberto Godoy 182, Cerrillos, Santiago de Chile</small>
             </div>
             <div class="h-100 d-inline-flex align-items-center">
-                <small class="far fa-clock text-primary me-2"></small>
+                <small class="far fa-clock text-warning me-2"></small>
                 <small>Atencion: Lunes a Jueves 8:30 a 16:00 hrs - Viernes de 8:30 a 15:00 hrs</small>
             </div>
         </div>
         <div class="col-lg-5 px-5 text-end">
             <div class="h-100 d-inline-flex align-items-center me-4">
-                <small class="fa fa-phone-alt text-primary me-2"></small>
+                <small class="fa fa-phone-alt text-warning me-2"></small>
                 <small>Telefono: (56 - 9) 4296 9687</small>
             </div>
             <div class="h-100 d-inline-flex align-items-center mx-n2">
@@ -76,7 +76,7 @@
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
     <a href="index.html" class="navbar-brand d-flex align-items-center border-end px-4 px-lg-5">
-        <h2 class="m-0 text-primary">Cristalerias Toro</h2>
+        <h2 class="m-0 text-black">Viveros Luanne</h2>
     </a>
     <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
@@ -84,7 +84,7 @@
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto p-4 p-lg-0">
             <a href="index.php" class="nav-item nav-link active">Inicio</a>
-            <a href="nada" class="nav-item nav-link">Quienes Somos</a>
+            <a href="quienes_somos.php" class="nav-item nav-link">Quienes Somos</a>
             <a href="codigoS.php" class="nav-item nav-link">Productos</a>
             <div class="nav-item dropdown">
             </div>
